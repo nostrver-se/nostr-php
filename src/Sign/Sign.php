edb198c63ca39456d5560c759655d72e95dd8cd6
pjv <pjv@users.noreply.github.com>
2023-03-28 06:27:02 -0500

    don’t re-encode note input

--- src/Sign/Sign.php
+++ src/Sign/Sign.php
@@ -25,7 +25,7 @@ class Sign
         $hash_content = $this->serializeEvent($event);
         if ($hash_content)
         {
-            $id = hash('sha256', utf8_encode($hash_content));
+            $id = hash('sha256', $hash_content);
             $event->setId($id);
 
             $sign = new SchnorrSignature();
