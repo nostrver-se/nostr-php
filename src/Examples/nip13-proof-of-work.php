09f8aa0b7cbf73820dfb64fad0b1c52f26bf7a47
Sebastix <info@sebastix.nl>
2025-05-27 16:16:13 +0200

    add try...catch and replace \n with PHP_EOL in this example

--- src/Examples/nip13-proof-of-work.php
+++ src/Examples/nip13-proof-of-work.php
@@ -15,24 +15,28 @@ use swentel\nostr\Sign\Sign;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$targetDifficulty = 21;
+try {
+    $targetDifficulty = 21;
 
-$event = new Event();
-$event->setKind(1);
-$event->setTags([]);
-$event->setContent('Hello, world!');
-$event->setCreatedAt(0);
-$event->setPublicKey('79c2cae114ea28a981e7559b4fe7854a473521a8d22a66bbab9fa248eb820ff6');
+    $event = new Event();
+    $event->setKind(1);
+    $event->setTags([]);
+    $event->setContent('Hello, world!');
+    $event->setCreatedAt(0);
+    $event->setPublicKey('79c2cae114ea28a981e7559b4fe7854a473521a8d22a66bbab9fa248eb820ff6');
 
-$event = Nip13Helper::minePow($event, $targetDifficulty);
+    $event = Nip13Helper::minePow($event, $targetDifficulty);
 
-$private_key = new Key();
-$private_key = $private_key->generatePrivateKey();
-$signer = new Sign();
-$signer->signEvent($event, $private_key);
+    $private_key = new Key();
+    $private_key = $private_key->generatePrivateKey();
+    $signer = new Sign();
+    $signer->signEvent($event, $private_key);
 
-if (Nip13Helper::getPowDifficulty($event->getId()) >= $targetDifficulty) {
-    print "Event mined successfully with difficulty $targetDifficulty " . $event->getId() . "\n";
-} else {
-    print "Event mining failed. Difficulty: " . Nip13Helper::getPowDifficulty($event->getId()) . "\n";
+    if (Nip13Helper::getPowDifficulty($event->getId()) >= $targetDifficulty) {
+        print "Event mined successfully with difficulty $targetDifficulty " . $event->getId() . PHP_EOL;
+    } else {
+        print "Event mining failed. Difficulty: " . Nip13Helper::getPowDifficulty($event->getId()) . PHP_EOL;
+    }
+} catch (\Exception $e) {
+    print 'Exception error: ' . $e->getMessage() . PHP_EOL;
 }
